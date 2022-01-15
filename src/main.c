f09ffe298e608093e9ffda65d651e29062202bde
John Jekel <JZJisawesome123@gmail.com>
2022-01-15 13:50:19 -0500

    Interactive code now works!

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /* Constants And Defines */
 
@@ -124,6 +125,8 @@ static void interactive(void)
 {
     interactive_intensity_gen_struct_t async_struct;
 
+    putchar('\n');
+
     //Prompt for info needed to generate intensities
     async_struct.config.x_pixels = interactive_prompt_for_uint("Enter the x resolution of the image (positive integer < 65536): ");
     async_struct.config.y_pixels = interactive_prompt_for_uint("Enter the y resolution of the image (positive integer < 65536): ");
@@ -137,19 +140,75 @@ static void interactive(void)
     thrd_t intensity_gen_thread;
     thrd_create(&intensity_gen_thread, &interactive_generate_intensities, (void*) &async_struct);
 
-    //bool
-
+    char* base_name = interactive_prompt_str("Enter the base file name you wish to create: ");
+    bool gen_bw = interactive_prompt_yn("Generate a black and white image? (Y/N): ");
+    bool gen_grey = interactive_prompt_yn("Generate a greyscale image? (Y/N): ");
+    bool gen_colour_8 = interactive_prompt_yn("Generate an 8-bit colour image? (Y/N): ");
+    bool gen_colour = interactive_prompt_yn("Generate a 24-bit colour image? (Y/N): ");
 
     //Wait for the intensities to finish generating, then async_struct.intensities will be valid!
     thrd_join(intensity_gen_thread, NULL);
 
-    //TESTING
-    bmp_t render;
-    mb_render_grey_8(async_struct.intensities, &render);
-    bmp_save(&render, "render_grey_8.bmp", BI_RLE8);
-    bmp_destroy(&render);
+    //TODO make this multithreaded + reuse code
+
+    if (gen_bw)
+    {
+        const char* suffix = "_bw.bmp";
+        char* file_name = malloc(sizeof(char) + (strlen(base_name) + strlen(suffix) + 1));
+        strcpy(file_name, base_name);
+        strcat(file_name, suffix);
+
+        bmp_t render;
+        mb_render_bw(async_struct.intensities, &render);
+        bmp_save(&render, file_name, BI_RGB);
+        bmp_destroy(&render);
+        free(file_name);
+    }
+
+    if (gen_grey)
+    {
+        const char* suffix = "_grey.bmp";
+        char* file_name = malloc(sizeof(char) + (strlen(base_name) + strlen(suffix) + 1));
+        strcpy(file_name, base_name);
+        strcat(file_name, suffix);
+
+        bmp_t render;
+        mb_render_grey_8(async_struct.intensities, &render);
+        bmp_save(&render, file_name, BI_RLE8);
+        bmp_destroy(&render);
+        free(file_name);
+    }
+
+    if (gen_colour_8)
+    {
+        const char* suffix = "_colour_8.bmp";
+        char* file_name = malloc(sizeof(char) + (strlen(base_name) + strlen(suffix) + 1));
+        strcpy(file_name, base_name);
+        strcat(file_name, suffix);
+
+        bmp_t render;
+        mb_render_colour_8(async_struct.intensities, &render);
+        bmp_save(&render, file_name, BI_RLE8);
+        bmp_destroy(&render);
+        free(file_name);
+    }
+
+    if (gen_colour)
+    {
+        const char* suffix = "_colour.bmp";
+        char* file_name = malloc(sizeof(char) + (strlen(base_name) + strlen(suffix) + 1));
+        strcpy(file_name, base_name);
+        strcat(file_name, suffix);
+
+        bmp_t render;
+        mb_render_colour(async_struct.intensities, &render);
+        bmp_save(&render, file_name, BI_RGB);
+        bmp_destroy(&render);
+        free(file_name);
+    }
 
     mb_destroy_intensities(async_struct.intensities);
+    free(base_name);
 }
 
 static uint16_t interactive_prompt_for_uint(const char* str)
@@ -181,6 +240,26 @@ static long double interactive_prompt_for_ld(const char* str)
     input_ld = strtold(input_buffer, NULL);
 
     return input_ld;
+}
+
+static char* interactive_prompt_str(const char* str)
+{
+    const size_t input_buffer_size = 1024;
+    char* input_buffer = (char*) malloc(sizeof(char) * input_buffer_size);
+
+    fputs(str, stdout);
+    fflush(stdout);
+
+    //Clear leading whitespace
+    char whitespace;
+    while (isspace(whitespace = getchar()));
+    ungetc(whitespace, stdin);
+
+    fgets(input_buffer, input_buffer_size, stdin);
+
+    input_buffer[strlen(input_buffer) - 1] = 0x00;//Remove the '\n'
+    input_buffer = (char*) realloc(input_buffer, sizeof(char) * (strlen(input_buffer) + 1));
+    return input_buffer;
 }
 
 static bool interactive_prompt_yn(const char* str)
